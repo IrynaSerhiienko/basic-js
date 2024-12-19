87c81491b4291f6b3174d2ba8d29aba14bfa1656
Iryna Serhiienko <irishka.sergienko@gmail.com>
2024-12-20 01:17:39 +0200

    feat: add new tasks

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam( members ) {
+	if(!Array.isArray(members)) return false;
+	const dreamTeamName = [];
+  members.forEach(name => {
+		if(typeof(name) !== 'string') return;
+		dreamTeamName.push((name.trim().slice(0, 1)).toUpperCase());
+		return dreamTeamName;
+	})
+	return dreamTeamName.sort().join('');
 }
 
 module.exports = {
